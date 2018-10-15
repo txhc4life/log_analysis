473ec9006bfe3894262a10b206ed0b2de6b35b1e
txhc4life <txhc4life@gmail.com>
2018-10-14 22:22:27 -0500

    Uploading project to github.

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-fullstack-nanodegree-vm
-=============
+### **log_analysis**
+log_analysis will create a report from a fictional news website database.
 
-Common code for the Relational Databases and Full Stack Fundamentals courses
+The database contains 3 tables. Articles, authors, and log. The log file contains web server log information such as the response code and time of the http request.
+
+The report will answer the following questions.
+
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
+
+The report will be written to a file named "news_report.txt" in your current working directory.
+
+### **Requirements**
+log_analysis uses Vagrant and Virtualbox to provide a virtual machine for dev/testing purposes.
+
+Git  
+`sudo apt-get install git`  
+Git for Windows  
+https://git-scm.com/download/win  
+Vagrant  
+https://www.vagrantup.com/downloads.html   
+Virtualbox  
+https://www.virtualbox.org/wiki/Downloads  
+Python 3 - Included in VM  
+https://www.python.org/downloads/  
+Postresql - Included in VM  
+https://www.postgresql.org/download/
+
+### **Instructions**
+Clone repository from github  
+`git clone https://github.com/txhc4life/log_analysis.git`
+
+Change the directory to the shared vagrant directory  
+`cd log_analysis/vagrant/``
+
+Start the VM  
+`vagrant up`
+
+SSH into the VM  
+`vagrant ssh`
+
+Change the directory to the shared vagrant directory  
+`cd /vagrant/`
+
+Import the database into postgresql  
+`psql -d news -f newsdata.sql`
+
+Install psycopg2  
+`pip3 install --user psycopg2-binary`
+
+Ensure the file is executable  
+`chmod +x log_analysis.py`
+
+Execute   
+`./log_analysis.py`
---
+++ log_analysis.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Prints report from the news database."""
+import psycopg2
+
+print("Starting report.")
+
+conn = psycopg2.connect("dbname=news")
+cur = conn.cursor()
+cur.execute("""
+            SELECT title, COUNT(title) AS views
+            FROM log, articles
+            WHERE replace(path, '/article/', '')  = slug
+            GROUP BY title
+            ORDER BY count(title)
+            DESC
+            LIMIT 3;
+            """)
+top_articles = cur.fetchall()
+
+cur.execute("""
+            SELECT name, COUNT(title) AS views
+            FROM authors, articles, log
+            WHERE replace(path, '/article/', '')  = slug
+            AND authors.id = articles.author
+            GROUP BY name
+            ORDER BY COUNT(title)
+            DESC;
+            """)
+top_authors = cur.fetchall()
+
+cur.execute("""
+           SELECT date(time) AS time,
+           (cast(COUNT(case when status = '404 NOT FOUND' then 1 end) AS float)
+           / COUNT(case when status = '200 OK' then 1 end)) * 100 as percentage
+           FROM log
+           GROUP BY date(time)
+           HAVING (cast(COUNT(case when status = '404 NOT FOUND' then 1 end)
+           AS float) / COUNT(case when status = '200 OK' then 1 end))
+           * 100 > 1.0;
+           """)
+days_with_errors = cur.fetchall()
+
+cur.close()
+conn.close()
+
+with open("news_report.txt", "w") as report_file:
+    print("Top Three Articles", file=report_file)
+    print("-------------------", file=report_file)
+    for i in range(len(top_articles)):
+        print('"%s" - %s views' % (top_articles[i][0], top_articles[i][1]),
+              file=report_file)
+
+    print("", file=report_file)
+    print("Most Popular Authors", file=report_file)
+    print("---------------------", file=report_file)
+    for i in range(len(top_authors)):
+        print('%s - %s views' % (top_authors[i][0], top_authors[i][1]),
+              file=report_file)
+
+    print("", file=report_file)
+    print("Days with more then 1% of errors", file=report_file)
+    print("---------------------------------", file=report_file)
+    for i in range(len(days_with_errors)):
+        print('%s : %.1f%%' % (days_with_errors[i][0], days_with_errors[i][1]),
+              file=report_file)
+
+print("Report is finished.")
+print("Filename: news_report.txt")
